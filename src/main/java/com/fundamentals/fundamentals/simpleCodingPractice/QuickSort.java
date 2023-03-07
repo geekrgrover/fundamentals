f2acaa70caa6060d21f37060e3be71f682d1ed30
geekrgrover <gopunair@outlook.com>
2023-03-07 12:22:37 -0800

    addimg some doc

--- src/main/java/com/fundamentals/fundamentals/simpleCodingPractice/QuickSort.java
+++ src/main/java/com/fundamentals/fundamentals/simpleCodingPractice/QuickSort.java
@@ -1,5 +1,12 @@
 package com.fundamentals.fundamentals.simpleCodingPractice;
 
+/**
+ * Quick sort
+ * Best case time complexity - O(n(log n))
+ * Worst case time complexity - O(n^2) (sorted input)
+ * space complexity - O(log(n))
+ * Not an In Place algorithm
+ */
 public class QuickSort {
 
     public static void main(String[] args) {
@@ -18,6 +25,16 @@ public class QuickSort {
         }
     }
 
+    /**
+     * Implementation of In-Place partitioning AKA Lumuto's partitioning.
+     * Helper function will be called up-front to get the work (sorting) done upfront.
+     * Merging will happen trivially as the sorting gets complete.
+     *
+     * @param list
+     * @param start
+     * @param end
+     * @return
+     */
     public static int qSortHelper(int[] list, int start, int end) {
         int smaller = start;
         for (int bigger = start + 1; bigger <= end; bigger++) {
@@ -26,10 +43,23 @@ public class QuickSort {
                 swap(smaller, bigger, list);
             }
         }
+        /** TIP: Here start is always the pivot. Ideally pivot can be picked arbitrarily using
+         * randomization on array indices.
+         * This can save the algorithm from predictability and provides protection from getting used against
+         * worst case data.
+         */
+        // Swap pivot with the left pointer to keep smaller values to the left of pivot and bigger to the right
         swap(start, smaller, list);
         return smaller;
     }
 
+    /**
+     * Helper function for swap.
+     *
+     * @param a
+     * @param b
+     * @param list
+     */
     private static void swap(int a, int b, int[] list) {
         int temp = 0;
         temp = list[a];
